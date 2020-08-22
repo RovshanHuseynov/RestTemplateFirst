866cdfa3c13514bcbae35bd808cb7e5e781b9bd1
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-08-22 15:05:19 +0400

    -- RestService class is updated

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//https://attacomsian.com/blog/http-requests-resttemplate-spring-boot
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
--- src/main/java/com/example/demo/service/RestService.java
+++ src/main/java/com/example/demo/service/RestService.java
@@ -2,8 +2,11 @@ package com.example.demo.service;
 
 import com.example.demo.model.Post;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
 
 @Service
 public class RestService {
@@ -13,18 +16,54 @@ public class RestService {
         this.restTemplate = restTemplateBuilder.build();
     }
 
+    // GET Request
     public String getPostsPlainJSON(){
         String url = "https://jsonplaceholder.typicode.com/posts";
         return this.restTemplate.getForObject(url, String.class);
     }
 
+    // Get Response as Object
     public Post[] getPostsAsObject(){
         String url = "https://jsonplaceholder.typicode.com/posts";
         return this.restTemplate.getForObject(url, Post[].class);
     }
 
+    // URL Parameters
     public Post getPostWithUrlParameters(){
         String url = "https://jsonplaceholder.typicode.com/posts/{id}";
         return this.restTemplate.getForObject(url, Post.class, 1);
     }
+
+    // Response Handing
+    public Post getPostWithResponseHandling(){
+        String url = "https://jsonplaceholder.typicode.com/posts/{id}";
+        ResponseEntity<Post> response = this.restTemplate.getForEntity(url, Post.class, 1);
+
+        if(response.getStatusCode() == HttpStatus.OK){
+            return response.getBody();
+        }
+        else{
+            return null;
+        }
+    }
+
+    // Custom Request Headers
+    public Post getPostWithCustomHeaders(){
+        String url = "https://jsonplaceholder.typicode.com/posts/{id}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.set("x-request-source", "desktop");
+
+        HttpEntity request = new HttpEntity(headers);
+        ResponseEntity<Post> response = this.restTemplate.exchange(url, HttpMethod.GET, request, Post.class, 1);
+        if(response.getStatusCode() == HttpStatus.OK){
+            return response.getBody();
+        }
+        else{
+            return null;
+        }
+    }
+
+
 }
