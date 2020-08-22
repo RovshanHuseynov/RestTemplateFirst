bd0fd4ca10faae0c1c808bb41b95248494aa5750
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-08-22 12:50:38 +0400

    -- service class is updated

--- src/main/java/com/example/demo/service/RestService.java
+++ src/main/java/com/example/demo/service/RestService.java
@@ -1,8 +1,11 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Post;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class RestService {
     private final RestTemplate restTemplate;
 
@@ -13,5 +16,15 @@ public class RestService {
     public String getPostsPlainJSON(){
         String url = "https://jsonplaceholder.typicode.com/posts";
         return this.restTemplate.getForObject(url, String.class);
+    }
+
+    public Post[] getPostsAsObject(){
+        String url = "https://jsonplaceholder.typicode.com/posts";
+        return this.restTemplate.getForObject(url, Post[].class);
+    }
+
+    public Post getPostWithUrlParameters(){
+        String url = "https://jsonplaceholder.typicode.com/posts/{id}";
+        return this.restTemplate.getForObject(url, Post.class, 1);
     }
 }
