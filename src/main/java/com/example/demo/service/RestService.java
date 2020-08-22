e84bae9795ff3cc24d5c669c60d2580e8b22c6fb
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-08-22 12:33:16 +0400

    -- model and service classes are implemented

--- src/main/java/com/example/demo/service/RestService.java
+++ src/main/java/com/example/demo/service/RestService.java
@@ -1,4 +1,17 @@
 package com.example.demo.service;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
+
 public class RestService {
+    private final RestTemplate restTemplate;
+
+    public RestService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public String getPostsPlainJSON(){
+        String url = "https://jsonplaceholder.typicode.com/posts";
+        return this.restTemplate.getForObject(url, String.class);
+    }
 }
